765622c23cf6f015fc17ea4287195583f306e2ec
Madi (LL) <mloui@mit.edu>
2024-12-13 16:50:08 -0500

    fix scripts

--- scripts/init_prototypes-simd-loop.sh
+++ scripts/init_prototypes-simd-loop.sh
@@ -10,6 +10,8 @@ module load anaconda/2023a
 source activate ovdsat
 
 DATA_DIR=data
+M=1
+
 for backbone in dinov2 #dinov2 clip-14 openclip-14 georsclip-14 remoteclip-14 clip-14-cap0 clip-14-cap1 clip-14-cap2 clip-14-gpt0-512-epoch23 clip-14-gpt0-512-epoch50 clip-14-gpte-512-epoch26 clip-14-gpte-512-epoch50 clip-14-gpt0-1024-epoch26 clip-14-gpt0-1024-epoch50 clip-14-gpte-1024-epoch24 clip-14-gpte-1024-epoch50
 do
     for dataset in simd
@@ -20,8 +22,8 @@ do
 
             python build_prototypes.py \
                 --data_dir ${DATA_DIR}/${dataset}/train \
-                --save_dir run/init_prototypes/boxes/${dataset}_N${N} \
-                --annotations_file ${DATA_DIR}/${dataset}/train_coco_subset_N${N}.json \
+                --save_dir run/init_prototypes/boxes/${dataset}_N${N}-${M} \
+                --annotations_file ${DATA_DIR}/${dataset}/train_coco_subset_N${N}-${M}.json \
                 --backbone_type ${backbone} \
                 --target_size 602 602 \
                 --window_size 224 \
